98282e329e74c5add4b95dd8e7850b0b6d9cd647
Johannes Buchner <johannes.buchner.acad@gmx.com>
2025-03-19 10:21:13 +0100

    add pyproject.toml because CI is complaining

--- setup.py
+++ setup.py
@@ -9,26 +9,7 @@ with open('README.rst') as f:
 	long_description = f.read()
 
 setup(
-	name='nway',
-	version='4.6.0',
-	author='Johannes Buchner',
-	author_email='johannes.buchner.acad@gmx.com',
 	packages=['nwaylib'],
 	scripts=['nway.py', 'nway-write-header.py', 'nway-explain.py', 'nway-create-fake-catalogue.py', 'nway-create-shifted-catalogue.py', 'nway-calibrate-cutoff.py'],
-	url='http://pypi.python.org/pypi/nway/',
-	license='AGPLv3 (see LICENSE file)',
-	description='Probabilistic Cross-Identification of Astronomical Sources',
 	long_description=long_description,
-	install_requires=[
-		"scipy",
-		"numpy",
-		"astropy",
-		"tqdm",
-		"matplotlib",
-		"joblib",
-		"healpy",
-		"pandas",
-	],
-	setup_requires=['pytest-runner'],
-	tests_require=['pytest'],
 )
